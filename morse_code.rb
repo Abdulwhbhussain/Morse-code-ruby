faf783fd12bd0b8b5d3e63d71eaaa90acd0161dd
Yout <youtlone95@gmail.com>
2023-08-23 17:46:11 +0630

    fix linter error

--- morse_code.rb
+++ morse_code.rb
@@ -1,63 +1,32 @@
 # Morse Code Converter
 # Morse Code Hash
 
-def decode_char(stringChar)
+def decode_char(string_char)
   morse_code = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z',
-    '.----' => '1',
-    '..---' => '2',
-    '...--' => '3',
-    '....-' => '4',
-    '.....' => '5',
-    '-....' => '6',
-    '--...' => '7',
-    '---..' => '8',
-    '----.' => '9',
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z', '.----' => '1', '..---' => '2', '...--' => '3', '....-' => '4',
+    '.....' => '5', '-....' => '6', '--...' => '7', '---..' => '8', '----.' => '9',
     '-----' => '0'
   }
-  morse_code[stringChar]
+  morse_code[string_char]
 end
 
-def decode_word(stringWord)
+def decode_word(string_word)
   word = ''
-  stringWord.split(' ').each do |char|
+  string_word.split.each do |char|
     word += decode_char(char)
   end
   word
 end
 
 def decode(string)
-  string = string.split('   ')
-  sentence = ''
-  string.each do |word|
-    sentence += decode_word(word) + ' '
-  end
-  sentence
+  string.split('   ').map do |word|
+    decode_word(word).strip
+  end.join(' ')
 end
 
 puts decode('.... . .-.. .-.. ---   .-- --- .-. .-.. -..')
